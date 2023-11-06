4b5953d6868550532910b2b4cde7845e88417ef1
porygonisaduck <alvaradx@umich.edu>
2023-11-05 22:41:21 -0500

    Single button for recipes

--- js/button.js
+++ js/button.js
@@ -1,19 +1,57 @@
+function eitherChange(name) {
+    button = document.getElementsByClassName(name)[0];
+    console.log(button);
+
+    if (button.firstChild.textContent == "Show More") {
+        button.firstChild.textContent = "Hide";
+        myShow(name);
+    }
+    else {
+        button.firstChild.textContent = "Show More";
+        myHide(name);
+    }
+}
+
 function myShow(name) {
     const hide = document.getElementsByClassName('hide');
-    for (let i = 0; i < hide.length; i+=2) {
+    
+    let defaults = 2;
+    for (let i = 0; i < hide.length; i+=defaults) {
+        console.log(i);
         if (hide[i].getAttribute('id') == name) {
             hide[i].style.display = "block";
-            hide[i+1].style.display = "block";
+            try {
+                hide[i+1].style.display = "block";
+            }
+            catch {
+                continue;
+            }
+
+        }
+
+        if (name.slice(0,6) == "recipe") {
+            defaults = 1;
         }
     }
 }
 
 function myHide(name) {
     const hide = document.getElementsByClassName('hide');
-    for (let i = 0; i < hide.length; i+=2) {
+
+    let defaults = 2;
+    for (let i = 0; i < hide.length; i+=defaults) {
         if (hide[i].getAttribute('id') == name) {
             hide[i].style.display = "none";
-            hide[i+1].style.display = "none";
+            try {
+                hide[i+1].style.display = "none";
+            }
+            catch {
+                continue;
+            }
+        }
+
+        if (name.slice(0,6) == "recipe") {
+            defaults = 1;
         }
     }
 }
